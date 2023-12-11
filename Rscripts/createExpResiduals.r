0cc4c5acfe870e7382267607cc9285a6f58e9350
Diogo Melo <diogro@gmail.com>
2023-12-11 14:04:01 -0500

    l2c not cpm

--- Rscripts/createExpResiduals.r
+++ Rscripts/createExpResiduals.r
@@ -1,15 +1,15 @@
 library(rio)
 source("Rscripts/functions.R")
 
-rnaseq_data = import("cache/rnaseq_all.rds")
+rnaseq_data = import("cache/rnaseq_all_2023-11-17.rds")
 
 treatment = pull(rnaseq_data$head$covariates, treatment)
-head_ctrl = rnaseq_data$head$cpm_residuals[,treatment == 1]
-head_hs = rnaseq_data$head$cpm_residuals[,treatment == 2]
+head_ctrl = rnaseq_data$head$mwash_residuals$l2c[,treatment == 0]
+head_hs   = rnaseq_data$head$mwash_residuals$l2c[,treatment == 1]
 
 treatment = pull(rnaseq_data$body$covariates, treatment)
-body_ctrl = rnaseq_data$body$cpm_residuals[,treatment == 1]
-body_hs = rnaseq_data$body$cpm_residuals[,treatment == 2]
+body_ctrl = rnaseq_data$body$mwash_residuals$l2c[,treatment == 0]
+body_hs   = rnaseq_data$body$mwash_residuals$l2c[,treatment == 1]
 
 write.table(head_ctrl, here::here("SBM/rawData/batch/head-ctrl.tsv"), sep = "\t")
 write.table(head_hs, here::here("SBM/rawData/batch/head-hs.tsv"), sep = "\t")
