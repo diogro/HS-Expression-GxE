891c8f9fdba8d1f3ea577652382c495a7d83ec3b
Diogo Melo <diogro@gmail.com>
2023-12-13 16:25:47 -0500

    reading only one phenotype

--- eQTLmapping/scripts/runGxEGridLMM.R
+++ eQTLmapping/scripts/runGxEGridLMM.R
@@ -17,11 +17,7 @@ colnames(X) = Xp$map$snp.name
 covariates = import(paste0("covariates/", tissue, ".tsv"))
 GRM = import(paste0("GRMs/", tissue, ".cXX.txt"), header = FALSE)
 colnames(GRM) = rownames(GRM) = covariates$id
-phenos = import(paste0("phenotypes/", tissue, ".tsv"))
 genes = import(paste0("phenotypes/", tissue, ".genes.txt"), header = FALSE)[,1]
-
-rownames(phenos) = genes
-phenos = t(phenos)
 
 global_formulas <- list(
           head = 
@@ -30,7 +26,7 @@ global_formulas <- list(
      y ~ 1 + egglayBatch + RNAseqBatch + platingBatch + RNAlibBatch + treatment + Zhat1 +         (1|id)
 )
 
-runGxEmodel = function(current_gene, tissue, covariates, phenos, GRM){
+runGxEmodel = function(current_gene, tissue, covariates, y, GRM){
      cache_folder = paste0('cache/',
                            tissue, '/',
                            current_gene)
@@ -42,23 +38,25 @@ runGxEmodel = function(current_gene, tissue, covariates, phenos, GRM){
      if (!dir.exists(cache_folder)) {
           dir.create(cache_folder, recursive = TRUE)
      }
+     y = t(import(paste0("phenotypes/", tissue, ".tsv"), skip = which(genes == current_gene)-1, nrows = 1))
      data = covariates |>
-          mutate(y = phenos[,current_gene]) |>
+          mutate(y = y) |>
           as.data.frame()
      rownames(data) = data$id
      V_setup = import(paste0(cache_folder, '/V_setup.rds'))
      gxe_gwas = GridLMM_GWAS(formula = global_formulas[[tissue]],
-                              test_formula =  ~1 + treatment,
-                              reduced_formula = ~1,
-                              data = data,
-                              X = X,
-                              X_ID = 'id',
-                              relmat = list(id = list(K = GRM)),
-                              V_setup = V_setup,  # This tells the function to reuse existing matrices
-                              method = 'REML',
-                              mc.cores = 2,
-                              verbose = T)
-     out_file = gxe_gwas$results |>
+                             test_formula =  ~1 + treatment,
+                             reduced_formula = ~1,
+                             data = data,
+                             X = X,
+                             X_ID = 'id',
+                             relmat = list(id = list(K = GRM)),
+                             V_setup = V_setup,
+                             method = 'REML',
+                             mc.cores = 2,
+                             verbose = T)
+     results = gxe_gwas$results
+     out_file = results |>
           mutate(Trait = current_gene) |>
           rename(snp = X_ID,
                  p_main = p_value_REML.1,
@@ -69,18 +67,17 @@ runGxEmodel = function(current_gene, tissue, covariates, phenos, GRM){
           as_tibble()
      export(out_file, results_file)
      if(nrow(out_file) > 0){
-          detection_file = paste0("detections/", current_gene, ".tsv")
+          detection_file = paste0("detections/", tissue, "/", current_gene, ".tsv")
           export(out_file, detection_file)
      } 
-     return(gxe_gwas$results)
+     return(results)
 }
-results = runGxEmodel(genes[2], tissue, covariates, phenos, GRM)
 
+runGxEmodel(genes[3], tissue, covariates, y, GRM)
 library(furrr)
 plan(multisession, workers = 32)
-
-results = future_map(genes, runGxEmodel, 
+options(future.globals.maxSize = 1e8 * 1024)
+results = future_map(genes[1:64], runGxEmodel, 
                      tissue = tissue, 
                      covariates = covariates,
-                     phenos = phenos,
                      GRM = GRM)
