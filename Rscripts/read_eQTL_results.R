080937b794e1b92c28c7b265f85161dc81ba8460
Diogo Melo <diogro@gmail.com>
2024-04-15 14:16:25 -0400

    cacheing pos graphs, exporting per gene clip degree

--- Rscripts/read_eQTL_results.R
+++ Rscripts/read_eQTL_results.R
@@ -6,7 +6,7 @@ detection_files = list(head = dir(here::here("eQTLmapping/detections/gxe/head"))
                           body = dir(here::here("eQTLmapping/detections/gxe/body")))              
 gxe_genes = lapply(detection_files, function(x) gsub(".tsv", "", x)) 
 
-tissue = 'head'
+tissue = 'body'
 
 Xp = read.plink(here::here(paste0("eQTLmapping/bed_files/", tissue)))
 X = as(Xp$genotypes,'numeric') 
@@ -75,76 +75,83 @@ for(i in k:length(genes)){
     results[[k]] = runGxEmodel(genes[i], tissue, covariates, GRM)
     k = k + 1
 }
-
+export(results, here::here(paste0("cache/eQTL_detections_gxe-", tissue, ".rds")))
 signCut <- 1e-4
 
 # Make a manhattan plot
 res = results[[1]]
+gene = genes[1]
+plotManhattan <- function(res, gene, signCut, tissue){
+    res = res |>
+        mutate(log_p_gxe = -log10(p_gxe)) |>
+        separate(snp, c("chr", "bp"), sep = "_") |>
+        mutate(chr = factor(chr, levels = chrs),
+                bp = as.numeric(bp)) |>
+        arrange(p_gxe) 
 
-res = res |>
-     mutate(log_p_gxe = -log10(p_gxe)) |>
-     separate(snp, c("chr", "bp"), sep = "_") |>
-     mutate(chr = factor(chr, levels = chrs),
-            bp = as.numeric(bp)) |>
-     arrange(p_gxe) 
+    gwas_data_load = res
+    sig_data <- gwas_data_load %>% 
+    subset(p_gxe < signCut)
+    notsig_data <- gwas_data_load %>% 
+    subset(p_gxe >= signCut) %>%
+    group_by(chr) %>% 
+    sample_frac(0.5)
+    gwas_data <- bind_rows(sig_data, notsig_data)
 
-gwas_data_load = res
-sig_data <- gwas_data_load %>% 
-  subset(p_gxe < signCut)
-notsig_data <- gwas_data_load %>% 
-  subset(p_gxe >= signCut) %>%
-  group_by(chr) %>% 
-  sample_frac(0.5)
-gwas_data <- bind_rows(sig_data, notsig_data)
+    data_cum <- gwas_data %>% 
+    group_by(chr) %>% 
+    summarise(max_bp = max(bp)) %>% 
+    mutate(bp_add = lag(cumsum(max_bp), default = 0)) %>% 
+    select(chr, bp_add)
 
-data_cum <- gwas_data %>% 
-  group_by(chr) %>% 
-  summarise(max_bp = max(bp)) %>% 
-  mutate(bp_add = lag(cumsum(max_bp), default = 0)) %>% 
-  select(chr, bp_add)
+    gwas_data <- gwas_data %>% 
+    inner_join(data_cum, by = "chr") %>% 
+    mutate(bp_cum = bp + bp_add)
 
-gwas_data <- gwas_data %>% 
-  inner_join(data_cum, by = "chr") %>% 
-  mutate(bp_cum = bp + bp_add)
+    axis_set <- gwas_data %>% 
+    filter(chr != 4) %>%
+    group_by(chr) %>% 
+    summarize(center = mean(bp_cum))
 
-axis_set <- gwas_data %>% 
-  filter(chr != 4) %>%
-  group_by(chr) %>% 
-  summarize(center = mean(bp_cum))
+    ylim <- gwas_data %>% 
+    filter(p_gxe == min(p_gxe)) %>% 
+    mutate(ylim = abs(floor(log10(p_gxe))) + 2) %>% 
+    pull(ylim)
 
-ylim <- gwas_data %>% 
-  filter(p_gxe == min(p_gxe)) %>% 
-  mutate(ylim = abs(floor(log10(p_gxe))) + 2) %>% 
-  pull(ylim)
-
-manhplot <- ggplot(filter(gwas_data, p_gxe < signCut, chr != 4), aes(x = bp_cum, y = -log10(p_gxe), 
-                                  color = chr)) +
-  geom_hline(yintercept = -log10(signCut), color = "grey40", linetype = "dashed") + 
-  geom_point(alpha = 0.7, size = 0.005) +
-  geom_point(data = filter(gwas_data, p_gxe > signCut), alpha = 0.75, size = 0.005, color = "grey") +
-  scale_x_continuous(label = axis_set$chr, breaks = axis_set$center) +
-  scale_y_continuous(expand = c(0,0), limits = c(0, ylim), breaks = seq(0, 10, 1)) +
-  scale_color_manual(values = rep(c("#276FBF", "#183059"), unique(length(axis_set$chr)))) +
-  scale_size_continuous(range = c(0.5,3)) +
-  labs(x = "Chromosome", 
-       y = expression("-log10(" ~ p[GxE] ~ ")")) + 
-  theme_classic() +
-  theme( 
-    legend.position = "none",
-    #panel.grid.major.x = element_blank(),
-    #panel.grid.minor.x = element_blank(),
-    #axis.title.y = element_markdown(),axis.line = element_line(color = 'black'), 
-    plot.title = element_text(size = 10), 
-    axis.text = element_text(size = 7), 
-    axis.text.x = element_text(size = 7, vjust = 0.5),
-    axis.title = element_text(size = 8),
-    legend.title = element_text(size = 8),
-    legend.text = element_text(size = 8)
-  )
-save_plot(here::here("tmp/gxe_manhattan.png"), manhplot)
-
-
-
+    manhplot <- ggplot(filter(gwas_data, p_gxe < signCut, chr != 4), aes(x = bp_cum, y = -log10(p_gxe), 
+                                    color = chr)) +
+    geom_hline(yintercept = -log10(signCut), color = "grey40", linetype = "dashed") + 
+    geom_point(alpha = 0.7, size = 0.005) +
+    geom_point(data = filter(gwas_data, p_gxe > signCut), alpha = 0.75, size = 0.005, color = "grey") +
+    scale_x_continuous(label = axis_set$chr, breaks = axis_set$center) +
+    scale_y_continuous(expand = c(0,0), limits = c(0, ylim), breaks = seq(0, 10, 1)) +
+    scale_color_manual(values = rep(c("#276FBF", "#183059"), unique(length(axis_set$chr)))) +
+    scale_size_continuous(range = c(0.5,3)) +
+    labs(x = "Chromosome", 
+        y = expression("-log10(" ~ p[GxE] ~ ")")) + 
+    theme_classic() +
+    theme( 
+        legend.position = "none",
+        #panel.grid.major.x = element_blank(),
+        #panel.grid.minor.x = element_blank(),
+        #axis.title.y = element_markdown(),axis.line = element_line(color = 'black'), 
+        plot.title = element_text(size = 10), 
+        axis.text = element_text(size = 7), 
+        axis.text.x = element_text(size = 7, vjust = 0.5),
+        axis.title = element_text(size = 8),
+        legend.title = element_text(size = 8),
+        legend.text = element_text(size = 8)
+    )
+    plot_folder = here::here(file.path("tmp/eQTL_GxE/plots", tissue))
+    # Check if exists and create plot_folder
+    if(!dir.exists(plot_folder)){
+    dir.create(plot_folder, recursive = TRUE)
+    }
+    save_plot(paste0(plot_folder, "/", gene, ".png"), manhplot, base_width = 7)
+    return(manhplot)
+}
+plotManhattan(res, gene, signCut, tissue)
+map2(results, genes, plotManhattan, signCut, tissue, .progress = "Manhattan Plots")
 
 
 
