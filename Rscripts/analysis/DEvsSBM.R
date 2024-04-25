bb567dc2f6e8def393d78fccb755662690499df3
Diogo Melo <diogro@gmail.com>
2024-04-25 14:20:32 -0400

    for real

--- Rscripts/analysis/DEvsSBM.R
+++ Rscripts/analysis/DEvsSBM.R
@@ -1,4 +1,4 @@
-source(here::here("Rscripts/functions.R"))
+source(here::here("Rscripts/functions.R")) 
 
 DiffExpAll = list(limma = import(here::here("output/HS-ctrl-DE_limma-table_2024-03-21.csv")),
                   vicar = import(here::here("output/HS-ctrl-DE_vicar-table_2024-03-21.csv")),
@@ -7,11 +7,11 @@ DiffExpAll = list(limma = import(here::here("output/HS-ctrl-DE_limma-table_2024-
 DiffExp = DiffExpAll$vicar %>% filter(tissue == "head") %>% as_tibble()
 
 head = import(here::here("SBM/snakemake/cache/blockSummary/fdr-1e-3/layered/head/gene_block.csv"))
-block_summary_clip = import(here::here("cache/blockSummary_clip_fdr-1e2_head.csv"))
+block_summary_clip = import(here::here("cache/clip/blockSummary_clip_fdr-1e2_head.csv"))
 
 block_summary = import(here::here("SBM/snakemake/cache/GO/fdr-1e-3/layered/head/blockSummary.csv")) 
 
-block_summary_clip = import(here::here("cache/blockSummary_clip_fdr-1e2_head.csv"))
+block_summary_clip = import(here::here("cache/clip/blockSummary_clip_fdr-1e2_head.csv"))
 block_summary_clip = block_summary_clip |> 
     filter(Nested_Level == 1, Direction == "decohere") |>
     arrange(Nested_Level, desc(Total_degree))
@@ -26,13 +26,30 @@ block_median$B2 = factor(block_median$B2, levels = block_median$B2)
 DiffSBM$B2 = factor(DiffSBM$B2, levels = block_median$B2)
 DE_blocks = block_median[block_median$median > 5,]
 
-DiffSBM_DE_blocks = DiffSBM %>% filter(B2 %in% DE_blocks$B2)
+clip_corrs = import(here::here("cache/long_clip.csv"))
 all_corrs = import(here::here("cache/long_corrs.csv")) 
+
+sortGenes <- function(x){
+    Gene1 = with(x, pmin(Gene1, Gene2))
+    Gene2 = with(x, pmax(Gene1, Gene2))
+    x$Gene1 = Gene1
+    x$Gene2 = Gene2
+    return(x)
+}
+clip_corrs = sortGenes(clip_corrs)
+all_corrs = sortGenes(all_corrs)
+
+clip_corrs <- left_join(clip_corrs, all_corrs, by = c("Gene1", "Gene2")) 
+
+DiffSBM_DE_blocks = DiffSBM %>% filter(B2 %in% DE_blocks$B2)
 DE_genes = DiffSBM_DE_blocks$Geneid
-gene1_DE = all_corrs$Gene1 %in% DE_genes
-gene2_DE = all_corrs$Gene2 %in% DE_genes
-all_corrs$DE = gene1_DE & gene2_DE
-DE_corrs = filter(all_corrs, DE) |> select(-DE)
+gene1_DE = clip_corrs$Gene1 %in% DE_genes
+gene2_DE = clip_corrs$Gene2 %in% DE_genes
+clip_corrs$DE = gene1_DE & gene2_DE
+DE_corrs = filter(clip_corrs, DE) |> select(-DE)
+
+
+
 
 classifyCorrBlock = function(x, blockSummary, level){   
     block_col = paste("B", level, sep = "")
@@ -40,7 +57,7 @@ classifyCorrBlock = function(x, blockSummary, level){
     gene2_block = blockSummary[[block_col]][match(x$Gene2, blockSummary$Gene)]
     return(gene1_block == gene2_block)
 }
-DE_corrs$B2 = classifyCorrBlock(DE_corrs, head_blocks, 2)
+DE_corrs$B2 = classifyCorrBlock(DE_corrs, head, 2)
 p = pivot_longer(DE_corrs, ctrl:hs, names_to = "treatment", values_to =  "correlation") |>
     ggplot(aes(B2, correlation, fill = treatment, group = interaction(treatment, B2))) + 
     geom_boxplot() 
--- Rscripts/analysis/corrVsSBM.R
+++ Rscripts/analysis/corrVsSBM.R
@@ -18,33 +18,46 @@ any(DE_genes == "FBgn0038484")
 
 head_blocks = import(here::here("SBM/snakemake/cache/blockSummary/fdr-1e-3/layered/head/gene_block.csv"))
 
-res_exp = list(ctrl = read.table(here::here("SBM/rawData/batch/head-ctrl.tsv"), sep = "\t"),
-               hs = read.table(here::here("SBM/rawData/batch/head-hs.tsv"), sep = "\t"))
+# res_exp = list(ctrl = read.table(here::here("SBM/rawData/batch/head-ctrl.tsv"), sep = "\t"),
+#                hs = read.table(here::here("SBM/rawData/batch/head-hs.tsv"), sep = "\t"))
 
-clip_df = import(here::here("cache/clip_fdr-1e2_head.csv")) |>
-    filter(Degree > 50)
-clip_genes = unique(clip_df$Gene)
-length(clip_genes)
+# clip_df = import(here::here("cache/clip_fdr-1e2_head.csv")) |>
+#     filter(Degree > 50)
+# clip_genes = unique(clip_df$Gene)
+# length(clip_genes)
 
-corrMatrices = list(ctrl = cor(t(res_exp$ctrl), method = "spearman"),
-                    hs = cor(t(res_exp$hs), method = "spearman"))
+# corrMatrices = list(ctrl = cor(t(res_exp$ctrl), method = "spearman"),
+#                     hs = cor(t(res_exp$hs), method = "spearman"))
 
-long_corrMatrices = lapply(corrMatrices, function(X) {
-    ind <- which(upper.tri(X, diag = TRUE), arr.ind = TRUE)
-    nn <- dimnames(X)
-    data.frame(row = nn[[1]][ind[, 1]],
-               col = nn[[2]][ind[, 2]],
-               val = X[ind]) %>% filter(val != 1.)
-})
-names(long_corrMatrices) = c("ctrl", "hs")
+# long_corrMatrices = lapply(corrMatrices, function(X) {
+#     ind <- which(upper.tri(X, diag = TRUE), arr.ind = TRUE)
+#     nn <- dimnames(X)
+#     data.frame(row = nn[[1]][ind[, 1]],
+#                col = nn[[2]][ind[, 2]],
+#                val = X[ind]) %>% filter(val != 1.)
+# })
+# names(long_corrMatrices) = c("ctrl", "hs")
 
-df_corrs = data.frame(long_corrMatrices$ctrl[,1:2], 
-                      ctrl = long_corrMatrices$ctrl$val, 
-                      hs = long_corrMatrices$hs$val) %>%
-                      rename("Gene1" = "row", "Gene2" = "col") %>%
-                      as_tibble()
-export(df_corrs, here::here("cache/long_corrs.csv"))
-df_corrs = import(here::here("cache/long_corrs.csv"))
+# df_corrs = data.frame(long_corrMatrices$ctrl[,1:2], 
+#                       ctrl = long_corrMatrices$ctrl$val, 
+#                       hs = long_corrMatrices$hs$val) %>%
+#                       rename("Gene1" = "row", "Gene2" = "col") %>%
+#                       as_tibble()
+# export(df_corrs, here::here("cache/long_corrs.csv"))
+clip_corrs = import(here::here("cache/long_clip.csv"))
+all_corrs = import(here::here("cache/long_corrs.csv")) 
+
+sortGenes <- function(x){
+    Gene1 = with(x, pmin(Gene1, Gene2))
+    Gene2 = with(x, pmax(Gene1, Gene2))
+    x$Gene1 = Gene1
+    x$Gene2 = Gene2
+    return(x)
+}
+clip_corrs = sortGenes(clip_corrs)
+all_corrs = sortGenes(all_corrs)
+
+clip_corrs <- left_join(clip_corrs, all_corrs, by = c("Gene1", "Gene2")) 
 
 classifyCorrBlock = function(x, blockSummary, level){   
     block_col = paste("B", level, sep = "")
@@ -53,11 +66,12 @@ classifyCorrBlock = function(x, blockSummary, level){
     return(gene1_block == gene2_block)
 }
 
-df_corrs$B2 = classifyCorrBlock(df_corrs, head_blocks, 2)
-gene1_DE = df_corrs$Gene1 %in% clip_genes
-gene2_DE = df_corrs$Gene2 %in% clip_genes
-df_corrs$DE = gene1_DE & gene2_DE
-table(df_corrs$DE)
+clip_corrs$sig = p.adjust(clip_corrs$pval, method = "BH") < 0.01
+
+clip_corrs$B2 = classifyCorrBlock(clip_corrs, head_blocks, 2)
+
+filter(clip_corrs, sig) |> select(-sig) |> export(affix_date(here::here("output/clip_correlations.csv")))
+
 # p = ggplot(df_corrs, aes(x = ctrl, y = hs)) + 
 #     geom_point(alpha = 0.1) + geom_abline(intercept = 0, slope = 1) + 
 #     geom_hline(yintercept = 0, linetype = "dashed") +
@@ -66,18 +80,18 @@ table(df_corrs$DE)
 #     theme_classic(12) + labs(x = "Correlation (ctrl)", y = "Correlation (hs)")
 # save_plot(here::here("output/corrVsSBM.png"), p, base_width = 10, base_height = 6)
 
-subsample = sample(nrow(df_corrs), 1000000)
+subsample = sample(nrow(clip_corrs), 1000000)
 
-p = ggplot(df_corrs, aes(x = ctrl, y = hs, group = interaction(DE, B2))) + 
+p = ggplot(clip_corrs, aes(x = ctrl, y = hs, group = interaction(sig, B2))) + 
         geom_hex(bins = 200) +
         scale_fill_viridis(option = "magma") +
         geom_abline(intercept = 0, slope = 1, color = 2) + 
         geom_hline(yintercept = 0, linetype = "dashed") +
         geom_vline(xintercept = 0, linetype = "dashed") +
-        facet_grid(B2~DE, labeller = labeller(B2 = c("FALSE" = "Between level-2 blocks", 
+        facet_grid(B2~sig, labeller = labeller(B2 = c("FALSE" = "Between level-2 blocks", 
                                                    "TRUE" = "Within level-2 blocks"),
-                                              DE = c("FALSE" = "Not Clip", 
-                                                     "TRUE" = paste0("Clip - ", length(clip_genes), " genes")))) +
+                                              sig = c("FALSE" = "Not Clip", 
+                                                     "TRUE" = paste0("Clip - ", table(clip_corrs$sig)[2], " genes")))) +
         theme_classic(12) + labs(x = "Correlation (ctrl)", y = "Correlation (hs)")
     
 save_plot(here::here("output/corrVsSigClip.png"), p, base_width = 10, base_height = 6)
